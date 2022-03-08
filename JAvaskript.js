830caa6bd6c5018437f7123b697c40c120fb0f67
Zuxriddin <example@gmail.com>
2022-03-08 21:20:02 +0500

    first commit

---
+++ JAvaskript.js
@@ -0,0 +1,92 @@
+"use strict";
+
+// const arr = {
+//   ism: "Zuxriddin",
+//   familiya: "Saidaxmadov",
+//   yosh: 18,
+//   Ish: "developer",
+// };
+// console.log(arr.yosh);
+
+// asdadaaaa
+
+// const date = [];
+
+// date[0] = prompt("ismingiz");
+// date[1] = prompt("familiyangiz");
+// date[2] = prompt("yoshingiz");
+// date[3] = prompt("Ishingiz");
+
+// console.log(date);
+
+// const firstName = +prompt("Yoshingiz");
+// console.log(10 + firstName);
+
+// const x = 10,
+//   y = 5;
+// const deck = true,
+//   inck = false;
+
+// console.log(deck && inck);
+
+// const obj = {
+//   kino: "",
+//   qachon: Number(21),
+//   arr: [],
+// };
+// obj.kino = prompt("qaysi kinoni ko'rdingiz");
+// obj.qachon = prompt("qachon ko'rdingiz");
+// obj.arr = "salom";
+
+// console.log(obj);
+
+// if (yosh >= 18) {
+//   console.log("kirish mumkin");
+// } else {
+//   console.log("mumkin emas");
+// }
+
+// yosh >= 18 ? console.log("mumkin") : console.log("mumkin emas");
+
+// const yosh = 20;
+
+// switch (yosh) {
+//   case 20:
+//     console.log("mumkin 20");
+//     break;
+//   case 18:
+//     console.log("mumkin 18");
+//     break;
+//   default:
+//     console.log("mumkin emas");
+// }
+
+// const soat = 9;
+
+// soat == 8 ? console.log("ochiqmiz") : console.log("biz yopildik");
+
+// switch (soat) {
+//   case (10, 9, 8):
+//     console.log("ofis ochiq");
+//     break;
+//   case 12:
+//     console.log("tushlik vaqti");
+//     break;
+//   default:
+//     console.log("ofis yopiq");
+// }
+
+let x = 0;
+
+// while (x < 10) {
+//   x++;
+//   if (x == 5) {
+//     continue;
+//   }
+//   console.log(x);
+// }
+
+do {
+  console.log(x);
+  x++;
+} while (x <= 10);
